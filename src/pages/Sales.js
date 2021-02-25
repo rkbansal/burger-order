8400fb039533c27465dde6a19d9dfc7d6db51817
raj bansal <raj.bansal@gameskraft.in>
2021-02-25 18:25:13 +0530

    default show all users

--- src/pages/Sales.js
+++ src/pages/Sales.js
@@ -4,9 +4,9 @@ import List from "../components/List";
 import Search from "../components/Search";
 import Header from "../components/Header";
 export default function Sales(){
-    const [filteredSales, setFilteredSales] = useState([]);
     let allSales = localStorage.getItem("sales") || "";
     allSales = JSON.parse(allSales);
+    const [filteredSales, setFilteredSales] = useState(allSales);
     return <div className="row column">
         <Search items={allSales} setItems={setFilteredSales}/>
         <Header headings={["name","salad","cheese","cutlet","total"]}></Header>
